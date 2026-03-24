038c02da2659e1de37c21ed0f7ac84c47cac6063
Tobias Dreifke <toobsecret@gmail.com>
2026-03-24 12:43:36 +0100

    refactor: urls

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from .views import (
     RegisterView, ActivateAccountView,
     LoginView, LogoutView,
+    CookieTokenRefreshView,
     PasswordResetRequestView, PasswordResetConfirmView,
 )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('activate/<str:uidb64>/<str:token>/', ActivateAccountView.as_view(), name='activate'),
+    path('activate/<str:uidb64>/<str:token>/',
+         ActivateAccountView.as_view(), name='activate'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
-    path('password-reset-confirm/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('password-reset/', PasswordResetRequestView.as_view(),
+         name='password-reset'),
+    path('password-reset-confirm/<str:uidb64>/<str:token>/',
+         PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 ]
