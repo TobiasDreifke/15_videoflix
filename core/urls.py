5a86cc0266f5d7fb4ffa4f677865e192cca625fd
Tobias Dreifke <toobsecret@gmail.com>
2026-03-30 14:47:48 +0200

    fiexed: urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('auth_app.api.urls')),
+    path('api/', include('auth_app.api.urls')),
     path('api/', include('video_app.api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
