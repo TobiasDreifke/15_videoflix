0f1ea2d5693f93aeaf51ad7ed5fd19af899ff632
Tobias Dreifke <toobsecret@gmail.com>
2026-03-30 14:47:40 +0200

    fixed: confirmed password

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -6,19 +6,19 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
-    repeated_password = serializers.CharField(write_only=True)
+    confirmed_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'repeated_password']
+        fields = ['email', 'password', 'confirmed_password']
 
     def validate(self, data):
-        if data['password'] != data['repeated_password']:
+        if data['password'] != data['confirmed_password']:
             raise serializers.ValidationError('Passwords do not match.')
         return data
 
     def create(self, validated_data):
-        validated_data.pop('repeated_password')
+        validated_data.pop('confirmed_password')
         user = User.objects.create_user(
             username=validated_data['email'],
             email=validated_data['email'],
