a8db1df8e3f9f796f8f09ece8780589734b17b84
Tobias Dreifke <toobsecret@gmail.com>
2026-03-30 21:55:22 +0200

    got some error checks

--- auth_app/utils.py
+++ auth_app/utils.py
@@ -1,8 +1,11 @@
+import logging
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 def send_activation_email(user, request):
@@ -10,12 +13,17 @@ def send_activation_email(user, request):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     frontend_url = settings.FRONTEND_URL
     activation_link = f"{frontend_url}/activate/{uid}/{token}/"
-    send_mail(
-        subject='Videoflix - Account aktivieren',
-        message=f'Klicke hier um deinen Account zu aktivieren: {activation_link}',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-    )
+    try:
+        send_mail(
+            subject='Videoflix - Account aktivieren',
+            message=f'Klicke hier um deinen Account zu aktivieren: {activation_link}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+        )
+        logger.info('Activation email sent to %s', user.email)
+    except Exception:
+        logger.exception('Failed to send activation email to %s', user.email)
+        raise
 
 
 def send_password_reset_email(user, request):
@@ -23,9 +31,14 @@ def send_password_reset_email(user, request):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     frontend_url = settings.FRONTEND_URL
     reset_link = f"{frontend_url}/reset-password/{uid}/{token}/"
-    send_mail(
-        subject='Videoflix - Passwort zurücksetzen',
-        message=f'Klicke hier um dein Passwort zurückzusetzen: {reset_link}',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-    )
+    try:
+        send_mail(
+            subject='Videoflix - Passwort zuruecksetzen',
+            message=f'Klicke hier um dein Passwort zurueckzusetzen: {reset_link}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+        )
+        logger.info('Password reset email sent to %s', user.email)
+    except Exception:
+        logger.exception('Failed to send password reset email to %s', user.email)
+        raise
