9a4b9ffb0d21a5b7b5c401fbe60c7587641896b5
Tobias Dreifke <toobsecret@gmail.com>
2026-04-07 10:11:44 +0200

    fix: added genre

--- video_app/models.py
+++ video_app/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+    def __str__(self):
+        return self.name
 
 
 class Video(models.Model):
