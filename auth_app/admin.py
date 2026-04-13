b1394a0f09e7f4c275b77910c35f959096eb0f9d
Tobias Dreifke <toobsecret@gmail.com>
2026-04-13 11:23:48 +0200

    added user

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    ordering = ('email',)
+    fieldsets = UserAdmin.fieldsets
