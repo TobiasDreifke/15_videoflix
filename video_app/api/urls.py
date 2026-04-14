acf9c4491b16d40776836e16f4e9c695f4ec98ca
Tobias Dreifke <toobsecret@gmail.com>
2026-04-14 14:43:56 +0200

    feat: added thumbnail view

--- video_app/api/urls.py
+++ video_app/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import VideoListView, VideoDetailView, HLSManifestView, HLSSegmentView
+from .views import (
+    VideoListView,
+    VideoDetailView,
+    ThumbnailDetailView,
+    HLSManifestView,
+    HLSSegmentView,
+)
 
 urlpatterns = [
     path('video/', VideoListView.as_view(), name='video-list'),
     path('video/<int:pk>/', VideoDetailView.as_view(), name='video-detail'),
+    path('video/<int:pk>/thumbnail/', ThumbnailDetailView.as_view(), name='video-thumbnail-detail'),
     path('video/<int:movie_id>/<str:resolution>/index.m3u8', HLSManifestView.as_view(), name='hls-manifest'),
     path('video/<int:movie_id>/<str:resolution>/<str:segment>/', HLSSegmentView.as_view(), name='hls-segment'),
 ]
