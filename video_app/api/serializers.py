6c6e908ad3465810eec36c7d4616453b2ac1aef5
Tobias Dreifke <toobsecret@gmail.com>
2026-04-14 14:44:30 +0200

    fix: thumbnail is now shown

--- video_app/api/serializers.py
+++ video_app/api/serializers.py
@@ -10,17 +10,24 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class VideoSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField()
+    thumbnail = serializers.SerializerMethodField()
     thumbnail_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Video
-        fields = ['id', 'created_at', 'title', 'description', 'thumbnail_url', 'category']
+        fields = ['id', 'created_at', 'title', 'description', 'thumbnail', 'thumbnail_url', 'category']
 
     def get_category(self, obj):
         return obj.genre.name if obj.genre else None
 
+    def get_thumbnail(self, obj):
+        return self._build_thumbnail_url(obj)
+
     def get_thumbnail_url(self, obj):
+        return self._build_thumbnail_url(obj)
+
+    def _build_thumbnail_url(self, obj):
         request = self.context.get('request')
         if obj.thumbnail and request:
-            return request.build_absolute_uri(obj.thumbnail.url)
+            return request.build_absolute_uri(f'/api/video/{obj.pk}/thumbnail/')
         return None
