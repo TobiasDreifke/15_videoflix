f0469165000fe21b1dacc9b5b5514a4db4a19d87
Tobias Dreifke <toobsecret@gmail.com>
2026-04-14 15:20:33 +0200

    feat: custom html mail for registration and reset pasword

--- auth_app/utils.py
+++ auth_app/utils.py
@@ -2,13 +2,38 @@
 
 import logging
 from urllib.parse import urlencode
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
+
+
+def send_templated_email(template_prefix, recipient, context):
+    """Render and send a branded multipart email from text and HTML templates."""
+
+    subject = render_to_string(f'emails/{template_prefix}_subject.txt', context).strip()
+    text_body = render_to_string(f'emails/{template_prefix}_body.txt', context)
+    html_body = render_to_string(f'emails/{template_prefix}_body.html', context)
+
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=text_body,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[recipient],
+    )
+    email.attach_alternative(html_body, 'text/html')
+    email.send()
+
+
+def build_logo_url():
+    """Return the absolute URL to the branded email logo asset."""
+
+    backend_url = settings.BACKEND_URL.rstrip('/')
+    return f'{backend_url}/static/auth_app/logo.png'
 
 
 def send_activation_email(user, request):
@@ -19,13 +44,19 @@ def send_activation_email(user, request):
     query = urlencode({'uid': uid, 'token': token})
     frontend_url = settings.FRONTEND_URL.rstrip('/')
     activation_link = f"{frontend_url}/pages/auth/activate.html?{query}"
+    context = {
+        'user': user,
+        'headline': 'Activate your Videoflix account',
+        'preheader': 'Confirm your email address to start using Videoflix.',
+        'cta_label': 'Activate Account',
+        'cta_url': activation_link,
+        'support_email': settings.DEFAULT_FROM_EMAIL,
+        'user_name': user.get_full_name() or user.username or user.email,
+        'videoflix_url': frontend_url,
+        'logo_url': build_logo_url(),
+    }
     try:
-        send_mail(
-            subject='Videoflix - Account aktivieren',
-            message=f'Klicke hier um deinen Account zu aktivieren: {activation_link}',
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[user.email],
-        )
+        send_templated_email('activation', user.email, context)
         logger.info('Activation email sent to %s', user.email)
     except Exception:
         logger.exception('Failed to send activation email to %s', user.email)
@@ -40,13 +71,19 @@ def send_password_reset_email(user, request):
     query = urlencode({'uid': uid, 'token': token})
     frontend_url = settings.FRONTEND_URL.rstrip('/')
     reset_link = f"{frontend_url}/pages/auth/confirm_password.html?{query}"
+    context = {
+        'user': user,
+        'headline': 'Reset your Videoflix password',
+        'preheader': 'Use the secure link below to choose a new password.',
+        'cta_label': 'Reset Password',
+        'cta_url': reset_link,
+        'support_email': settings.DEFAULT_FROM_EMAIL,
+        'user_name': user.get_full_name() or user.username or user.email,
+        'videoflix_url': frontend_url,
+        'logo_url': build_logo_url(),
+    }
     try:
-        send_mail(
-            subject='Videoflix - Passwort zuruecksetzen',
-            message=f'Klicke hier um dein Passwort zurueckzusetzen: {reset_link}',
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[user.email],
-        )
+        send_templated_email('password_reset', user.email, context)
         logger.info('Password reset email sent to %s', user.email)
     except Exception:
         logger.exception('Failed to send password reset email to %s', user.email)
